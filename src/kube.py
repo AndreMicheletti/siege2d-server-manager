1b450c853b9d6122a53f18f6f03c078be2f713dc
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2021-07-09 23:13:40 -0300

    :rocket: seems like a stable version. deploying

--- src/kube.py
+++ src/kube.py
@@ -14,20 +14,29 @@ from pprint import pprint
 CLUSTER_HOST = os.getenv("CLUSTER_HOST")
 CLUSTER_TOKEN = os.getenv("CLUSTER_TOKEN")
 
+configuration = client.Configuration()
+# Configure API key authorization: BearerToken
+configuration.api_key['authorization'] = CLUSTER_TOKEN
+configuration.api_key_prefix['authorization'] = 'Bearer'
+
+# Defining host is optional and default to http://localhost
+configuration.host = CLUSTER_HOST
+configuration.verify_ssl = False
+
 def get_available_game_servers_ordered() -> list:
-  api_client = get_client_from_token()
-  game_servers = list_game_servers(api_client)
-  if game_servers:
-    available = [
-      {
-        **gs["status"],
-        "port": gs["status"]["ports"][0]["port"]
-      } for gs in game_servers["items"] if gs["status"]["state"] == "Ready"
-    ]
-    pprint(available)
-    return list(sorted(available, key=lambda x: x["port"]))
-  else:
-    return []
+	with client.ApiClient(configuration) as api_client:
+		game_servers = list_game_servers(api_client)
+		if game_servers:
+			available = [
+				{
+					**gs["status"],
+					"port": gs["status"]["ports"][0]["port"]
+				} for gs in game_servers["items"] if gs["status"]["state"] == "Ready"
+			]
+			pprint(available)
+			return list(sorted(available, key=lambda x: x["port"]))
+		else:
+			return []
 
 def list_game_servers(api_client: client.ApiClient) -> list:
     # Create an instance of the API class
@@ -46,11 +55,3 @@ def list_game_servers(api_client: client.ApiClient) -> list:
 def get_client_from_config():
   config.load_kube_config(context="minikube")
   return client.ApiClient()
-
-def get_client_from_token():
-    token = CLUSTER_TOKEN
-    cluster_config = client.Configuration()
-    cluster_config.host = CLUSTER_HOST
-    cluster_config.verify_ssl = False
-    cluster_config.api_key = {"authorization": "Bearer " + token}
-    return client.ApiClient(cluster_config)
